10df9831d27c48cfebd6f08c098eede5a132d7bd
Kamal Rathore <mr.rathoreji52@gmail.com>
2026-01-30 18:59:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,11 +2,10 @@ function insert_Row() {
     let table = document.querySelector("#sampleTable");
 	let row = document.createElement("tr");
 	let td1 = document.createElement("td");
-	td1.textContent = "New cell1";
+	td1.textContent = "New Cell1";
 	let td2 = document.createElement("td");
-	td2.textContent = "New cell2";
+	td2.textContent = "New Cell2";
 	row.append(td1);
 	row.append(td2);
 	table.prepend(row);
-  
 }
